f9d69c177047a1798d9396e8bb5e9fa0e3847acb
isudzumi <isudzumi@gmail.com>
2017-11-05 23:15:35 +0900

    Add .gitconfig to install scripts

--- install.bat
+++ install.bat
@@ -1,4 +1,6 @@
 @echo off
+rem encoding cp932
 rem ä«óùé“å†å¿Ç≈é¿çs
 mklink %HOME%\.vimrc %HOME%\dotfiles\.vimrc
+mklink %HOME%\.gitconfig %HOME%\dotfiles\.gitconfig
 mklink /D %HOME%\.vim %HOME%\dotfiles\.vim
--- install.ps1
+++ install.ps1
@@ -2,6 +2,7 @@
 try{
     ni -Path $HOME -Name .vimrc -Value .vimrc -ItemType SymbolicLink
     ni -Path $HOME -Name .vim -Value .vim -ItemType SymbolicLink
+    ni -Path $HOME -Name .gitconfig -Value .vim -ItemType SymbolicLink
 } catch {
     Write-Error("ÉGÉâÅ[" + $_.Error)
 } 
