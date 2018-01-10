5a8f4f9eecd930a9baa55221f8c867485079927b
isudzumi <isudzumi@gmail.com>
2018-01-10 13:12:07 +0900

    Change env

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 @echo off
 rem encoding cp932
 rem ä«óùé“å†å¿Ç≈é¿çs
-mklink %HOME%\.vimrc %HOME%\dotfiles\.vimrc
-mklink %HOME%\.gitconfig %HOME%\dotfiles\.gitconfig
-mklink /D %HOME%\.vim %HOME%\dotfiles\.vim
+mklink %USERPROFILE%\.vimrc %USERPROFILE%\dotfiles\.vimrc
+mklink %USERPROFILE%\.gitconfig %USERPROFILE%\dotfiles\.gitconfig
+mklink /D %USERPROFILE%\.vim %USERPROFILE%\dotfiles\.vim
