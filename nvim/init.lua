7410466e289b08583ae851fc1e297fc5bad58e65
Hiroaki ITO <mail@isudzumi.com>
2021-08-15 22:11:10 +0900

    [nvim] Setup nvim with lua plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,10 @@
 require('plugins')
+
+vim.o.nu = true
+vim.o.shiftwidth = 4
+vim.o.completeopt = 'menuone,noinsert,noselect'
+vim.o.shortmess = vim.o.shortmess .. 'c'
+
+-- For JIS keyboard, that assigns semicolon to the right little finger position
+vim.api.nvim_set_keymap('n', ':', ';', { noremap=true, silent=true })
+vim.api.nvim_set_keymap('n', ';', ':', { noremap=true, silent=true })
