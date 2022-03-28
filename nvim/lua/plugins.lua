6fbab061f0b1b46f563e3b3a52be2c8a775fb306
Hiroaki ITO <mail@isudzumi.com>
2022-03-28 15:53:00 +0900

    [nvim] Add tree-sitter develop env

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -2,6 +2,7 @@ require('packer').startup(function()
     use 'wbthomason/packer.nvim'
     use {'neovim/nvim-lspconfig'}
     use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+    use {'nvim-treesitter/playground'}
     use {'nvim-lua/completion-nvim', event = 'VimEnter' }
     use {'nvim-telescope/telescope.nvim',
         event = 'VimEnter',
@@ -51,7 +52,34 @@ require'nvim-treesitter.configs'.setup {
 	    scope_incremental = "grc",
 	    node_decremental = "grm",
 	}
+    },
+    playground = {
+      enable = true,
+      disable = {},
+      updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+      persist_queries = false, -- Whether the query persists across vim sessions
+      keybindings = {
+        toggle_query_editor = 'o',
+        toggle_hl_groups = 'i',
+        toggle_injected_languages = 't',
+        toggle_anonymous_nodes = 'a',
+        toggle_language_display = 'I',
+        focus_language = 'f',
+        unfocus_language = 'F',
+        update = 'R',
+        goto_node = '<cr>',
+        show_help = '?',
+      },
     }
+}
+
+local parser_config = require 'nvim-treesitter.parsers'.get_parser_configs()
+parser_config.vcl = {
+    install_info = {
+        url = "~/repo/github.com/isudzumi/tree-sitter-vcl",
+        files = {"src/parser.c"}
+    },
+    filetype = "vcl"
 }
 
 local nvim_lsp = require('lspconfig')
