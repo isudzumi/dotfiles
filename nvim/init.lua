46ae77c95b601fe2309f379a24bf6dcd4a9a9279
Hiroaki ITO <mail@isudzumi.com>
2022-04-18 15:23:17 +0900

    [nvim] Add keymap for terminal mode

--- nvim/init.lua
+++ nvim/init.lua
@@ -25,3 +25,5 @@ vim.o.clipboard = vim.o.clipboard .. 'unnamedplus'
 -- For JIS keyboard, that assigns semicolon to the right little finger position
 vim.api.nvim_set_keymap('n', ':', ';', { noremap=true, silent=true })
 vim.api.nvim_set_keymap('n', ';', ':', { noremap=true, silent=true })
+
+vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap=true, silent=true })
