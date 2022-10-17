d21801e7cb08b40b3d002d941d13fab041b94958
Hiroaki ITO <mail@isudzumi.com>
2022-10-17 16:36:11 +0900

    [nvim] Replace from update_capabilities with default_capabilities due to deprecation

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -33,7 +33,7 @@ local on_attach = function(client, bufnr)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
